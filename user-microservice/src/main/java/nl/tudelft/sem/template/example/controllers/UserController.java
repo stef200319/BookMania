e3a48ca533c09ba8e5e08ecbdc9cda845e7c2166
KiloBGram <51906114+KiloBGram@users.noreply.github.com>
2023-12-22 16:30:41 +0200

    Added searchUser entity

--- user-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/UserController.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/UserController.java
@@ -1,12 +1,17 @@
 package nl.tudelft.sem.template.example.controllers;
 
+import java.util.List;
 import nl.tudelft.sem.template.api.UserApi;
-import nl.tudelft.sem.template.example.model.User;
+import nl.tudelft.sem.template.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController implements UserApi {
+
+  public ResponseEntity<List<User>> searchUser(String query, String searchBy, Boolean isAuthor) {
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
 
 }
