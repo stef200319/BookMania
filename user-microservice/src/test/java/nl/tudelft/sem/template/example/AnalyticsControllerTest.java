b7c0c6ac8ffae20bd43d0b9e0f0005790710629b
pauls <P.G.Stan@student.tudelft.nl>
2024-01-09 12:52:49 +0100

    Created part of the tests for the analytics endpoint.

---
+++ user-microservice/src/test/java/nl/tudelft/sem/template/example/AnalyticsControllerTest.java
@@ -0,0 +1,108 @@
+package nl.tudelft.sem.template.example;
+
+import nl.tudelft.sem.template.example.controllers.AnalyticsController;
+import nl.tudelft.sem.template.example.database.AnalyticsRepository;
+import nl.tudelft.sem.template.example.database.UserRepository;
+import nl.tudelft.sem.template.example.model.Analytics;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AnalyticsControllerTest {
+    private AnalyticsController analyticsController;
+    private AnalyticsRepository analyticsRepository;
+    private UserRepository userRepository;
+    @BeforeEach
+    void setUp() {
+        analyticsRepository = Mockito.mock(AnalyticsRepository.class);
+        userRepository = Mockito.mock(UserRepository.class);
+        analyticsController = new AnalyticsController(userRepository, analyticsRepository);
+    }
+
+    @Test
+    void createAnalyticsInvalidUserTest() {
+        String username = "testUsername";
+
+        Analytics analytics = new Analytics(username, "now");
+
+        Mockito.when(userRepository.existsById(username)).thenReturn(false);
+
+        ResponseEntity response = analyticsController.createAnalyticsEntity(username, analytics);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Username does not exist.", response.getBody());
+
+        Mockito.verify(userRepository, Mockito.times(1)).existsById(username);
+    }
+
+    @Test
+    void existingAnalyticsTest() {
+        String username = "testUsername";
+
+        Analytics analytics = new Analytics(username, "now");
+
+        Mockito.when(userRepository.existsById(username)).thenReturn(true);
+        Mockito.when(analyticsRepository.existsById(username)).thenReturn(true);
+
+        ResponseEntity response = analyticsController.createAnalyticsEntity(username, analytics);
+
+        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
+        assertEquals("Analytics entity already exists.", response.getBody());
+
+        Mockito.verify(userRepository, Mockito.times(1)).existsById(username);
+        Mockito.verify(analyticsRepository, Mockito.times(1)).existsById(username);
+    }
+
+    @Test
+    void editAnalyticsBadTest() {
+        String username = "testUsername";
+
+        Analytics analytics = new Analytics(username, "now");
+
+        Mockito.when(analyticsRepository.existsById(username)).thenReturn(false);
+
+        ResponseEntity response = analyticsController.editAnalyticsEntity(username, analytics);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Analytics entity does not exist.", response.getBody());
+
+        Mockito.verify(analyticsRepository, Mockito.times(1)).existsById(username);
+    }
+
+    @Test
+    void getAnalyticsBadTest() {
+        String username = "testUsername";
+
+        // Analytics analytics = new Analytics(username, "now");
+
+        Mockito.when(analyticsRepository.existsById(username)).thenReturn(false);
+
+        ResponseEntity response = analyticsController.getAnalyticsEntity(username);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Analytics entity does not exist.", response.getBody());
+
+        Mockito.verify(analyticsRepository, Mockito.times(1)).existsById(username);
+    }
+
+    @Test
+    void deleteAnalyticsBadTest() {
+        String username = "testUsername";
+
+        // Analytics analytics = new Analytics(username, "now");
+
+        Mockito.when(analyticsRepository.existsById(username)).thenReturn(false);
+
+        ResponseEntity response = analyticsController.deleteAnalyticsEntity(username);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals("Analytics entity does not exist.", response.getBody());
+
+        Mockito.verify(analyticsRepository, Mockito.times(1)).existsById(username);
+    }
+}
