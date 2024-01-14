928aca205032e33fd8bfc305c15490e6989dacf4
Marius Frija <m.frija@student.tudelft.nl>
2024-01-14 23:08:21 +0100

    Created and integrated 2 new Exception subclassses

--- user-microservice/src/main/java/nl/tudelft/sem/template/example/bookHandlers/BookValidator.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/bookHandlers/BookValidator.java
@@ -1,9 +1,11 @@
 package nl.tudelft.sem.template.example.bookHandlers;
 
+import nl.tudelft.sem.template.example.exceptions.InvalidAuthorException;
 import nl.tudelft.sem.template.example.exceptions.InvalidBookException;
+import nl.tudelft.sem.template.example.exceptions.InvalidBookIdException;
 import nl.tudelft.sem.template.example.model.Book;
 
 public interface BookValidator {
     void setNext(BookValidator handler);
-    boolean handle(Book book) throws InvalidBookException;
+    boolean handle(Book book) throws InvalidBookException, InvalidAuthorException, InvalidBookIdException;
 }
