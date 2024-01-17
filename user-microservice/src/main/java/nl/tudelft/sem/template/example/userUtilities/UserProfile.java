be23299be71d111ae30d95ba542e6bd28ee56148
Aykut Emre Celen <a.e.celen@student.tudelft.nl>
2024-01-17 11:55:28 +0100

    Fixing checkstyle errors for user utilities

--- user-microservice/src/main/java/nl/tudelft/sem/template/example/userUtilities/UserProfile.java
+++ user-microservice/src/main/java/nl/tudelft/sem/template/example/userUtilities/UserProfile.java
@@ -1,16 +1,15 @@
 package nl.tudelft.sem.template.example.userUtilities;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import java.util.List;
+import java.util.Objects;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Setter
 @Getter
@@ -32,12 +31,22 @@ public class UserProfile {
     @Valid
     @ElementCollection
     private List<String> favoriteGenres;
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         UserProfile that = (UserProfile) o;
-        return Objects.equals(username, that.username) && Objects.equals(bio, that.bio) && Objects.equals(location, that.location) && Objects.equals(profilePicture, that.profilePicture) && Objects.equals(favoriteBook, that.favoriteBook) && Objects.equals(favoriteGenres, that.favoriteGenres);
+        return Objects.equals(username, that.username)
+                && Objects.equals(bio, that.bio)
+                && Objects.equals(location, that.location)
+                && Objects.equals(profilePicture, that.profilePicture)
+                && Objects.equals(favoriteBook, that.favoriteBook)
+                && Objects.equals(favoriteGenres, that.favoriteGenres);
     }
 
     @Override
@@ -47,13 +56,13 @@ public class UserProfile {
 
     @Override
     public String toString() {
-        return "UserProfile{" +
-                "username='" + username + '\'' +
-                ", bio='" + bio + '\'' +
-                ", location='" + location + '\'' +
-                ", profilePicture='" + profilePicture + '\'' +
-                ", favoriteBook='" + favoriteBook + '\'' +
-                ", favoriteGenres=" + favoriteGenres +
-                '}';
+        return "UserProfile{"
+                + "username='" + username + '\''
+                + ", bio='" + bio + '\''
+                + ", location='" + location + '\''
+                + ", profilePicture='" + profilePicture + '\''
+                + ", favoriteBook='" + favoriteBook + '\''
+                + ", favoriteGenres=" + favoriteGenres
+                + '}';
     }
 }
